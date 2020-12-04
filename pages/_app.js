b010a23474beef71c6fe2b025228a77b78344e4f
Eduardo Diaz <lalo.diaz1896@gmail.com>
2020-12-04 00:57:49 -0600

    [fix] global styled components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-// import '../styles.css'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    margin: 0;
+    background: papayawhip;
+  }
+`
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,11 @@
-import React from 'react'
 import styled from 'styled-components'
 
+const Title = styled.h1`
+    font-size: 5em;
+    color: ${({ theme }) => theme.colors.primary};
+    padding: 3em;
+`
 
-const index = () => {
-    const Title = styled.h1`
-        font-size: 1.5em;
-        text-align: center;
-        color: palevioletred;
-`;
-
-    return (
-        <div>
-            <Title>
-                <h1>Eduardo Díaz</h1>
-            </Title>
-            <h3>Frontend Developer y UI Designer</h3>
-        </div>
-    )
+export default function index() {
+    return <Title>Hola</Title>
 }
-
-export default index
