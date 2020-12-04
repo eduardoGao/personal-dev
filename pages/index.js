4241e8e6f706dd3da7508248f838b812fd1481d2
Eduardo Diaz <lalo.diaz1896@gmail.com>
2020-12-03 23:28:24 -0600

    [add] global styles

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
+
 
 const index = () => {
+    const Title = styled.h1`
+        font-size: 1.5em;
+        text-align: center;
+        color: palevioletred;
+`;
+
     return (
         <div>
-            <h1>Eduardo Díaz</h1>
+            <Title>
+                <h1>Eduardo Díaz</h1>
+            </Title>
             <h3>Frontend Developer y UI Designer</h3>
         </div>
     )
